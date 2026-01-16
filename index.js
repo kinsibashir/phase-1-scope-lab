5c7ceccef5c3d78b44d905d39c0b8b8a61360ecb
KINSI BASHIR <@@kinsibashir@gmail.com>
2026-01-16 13:54:45 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+// global variable
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newCustomer) {
+  window.bestCustomer = newCustomer;
+}
+
+// constant variable
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  // this MUST throw an error
+  leastFavoriteCustomer = 'someone else';
+}
